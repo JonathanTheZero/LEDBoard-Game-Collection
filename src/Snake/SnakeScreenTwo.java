60ac681d1843c120cea89c49d78d6bdebd6a4004
Jonathan Frohne <jonathan.frohne@web.de>
2017-11-18 11:25:39 +0100

    Fixed the problem, that the snake igonores the border

--- src/Snake/SnakeScreenTwo.java
+++ src/Snake/SnakeScreenTwo.java
@@ -1,17 +1,17 @@
 package Snake;
 
 import java.awt.event.KeyEvent;
+
 import ledControl.BoardController;
 import ledControl.gui.KeyBuffer;
 
-
-public class SnakeScreenOne {
+public class SnakeScreenTwo {
 	private static BoardController	controller;
 	private static KeyBuffer		buffer;
 	private static int xPos, yPos;
 	int snakeLength = 1;
 	
-	public SnakeScreenOne(){
+	public SnakeScreenTwo(){
 		controller = BoardController.getBoardController();
 		buffer = controller.getKeyBuffer();
 		xPos = 5;
@@ -22,37 +22,37 @@ public class SnakeScreenOne {
 				if (event.getID() == java.awt.event.KeyEvent.KEY_PRESSED){
 					switch (event.getKeyCode()){
 					case java.awt.event.KeyEvent.VK_UP:
-						if (yPos > 1){
+						if (yPos > 0){
 							yPos--;
 						}
-						else if (yPos <= 1){
+						else if (yPos <= 0){
 							yPos = 5;
 							xPos = 5;
 						}
 						break;
 					case java.awt.event.KeyEvent.VK_DOWN:
-						if (yPos < 10){
+						if (yPos < 11){
 							yPos++;
 						}
-						else if (yPos >= 10){
+						else if (yPos >= 11){
 							yPos = 5;
 							xPos = 5;
 						}
 						break;
 					case java.awt.event.KeyEvent.VK_LEFT:
-						if (xPos > 1){
+						if (xPos > 0){
 							xPos--;
 						}
-						else if (xPos <= 1){
+						else if (xPos <= 0){
 							yPos = 5;
 							xPos = 5;
 						}
 						break;
 					case java.awt.event.KeyEvent.VK_RIGHT:
-						if (xPos < 10){
+						if (xPos < 11){
 							xPos++;
 						}
-						else if (xPos >= 10){
+						else if (xPos >= 11){
 							yPos = 5;
 							xPos = 5;
 						}
@@ -69,26 +69,5 @@ public class SnakeScreenOne {
 	public void resetSnake() {
 		controller.resetColors();
 		snakeLength = 1;
-		int i = 0;
-		while (i <= 11) {
-			controller.setColor(0, i, 127, 0, 0);
-			i++;
-		}
-		i = 0;
-		while (i <= 10) {
-			controller.setColor(i, 0, 127, 0, 0);
-			i++;
-		}
-		i = 0;
-		while (i <= 11) {
-			controller.setColor(11, i, 127, 0, 0);
-			i++;
-		}
-		i = 0;
-		while (i <= 10) {
-			controller.setColor(i, 11, 127, 0, 0);
-			i++;
-		}
 	}
-
 }
